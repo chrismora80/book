1153a018ce1a85f7ae0be1fff6f140d96c637132
Christian Mora <christianmora007@hotmail.com>
2020-07-29 14:30:06 -0400

    upda

---
+++ client/build/precache-manifest.10e8072b371b4eaf6bd2cd28b83ba63c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f65fcb2ac2599e50cfe8",
+    "url": "/static/css/main.05f4b873.chunk.css"
+  },
+  {
+    "revision": "f65fcb2ac2599e50cfe8",
+    "url": "/static/js/main.2eef5f31.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7df09a61a2c37d2f3a9",
+    "url": "/static/js/2.5c25870c.chunk.js"
+  },
+  {
+    "revision": "a6488329d556f205c1d533a6c702fc96",
+    "url": "/index.html"
+  }
+];
